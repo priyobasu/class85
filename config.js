719df9a691c7f50f201ff31c46d68c37d49b0712
priyobasu <72121579+priyobasu@users.noreply.github.com>
2021-09-17 20:06:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+ 
+ 
+ export const firebaseConfig = {
+    apiKey: "AIzaSyA15CKd8ss7YOImi1ztA5OGotnJ7IsiVWE",
+    authDomain: "story-telling-app-1-45d9f.firebaseapp.com",
+    projectId: "story-telling-app-1-45d9f",
+    storageBucket: "story-telling-app-1-45d9f.appspot.com",
+    messagingSenderId: "23452680995",
+    appId: "1:23452680995:web:e7d9c524d6cf0ec2420296",
+    measurementId: "G-6C9S8C8XMT"
+  };
